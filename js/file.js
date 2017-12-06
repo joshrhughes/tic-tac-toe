9dea4d7c936365bdb224b53c22a765a37c987120
joshrhughes <josh@mtnpine.us>
2017-12-06 16:29:17 -0700

    A late commit for starter code

---
+++ js/file.js
@@ -0,0 +1,15 @@
+// console.log("js works");
+
+
+
+$().ready(function(){
+	console.log("ready!");
+	$( ".cell" ).click(function() {
+	  		$( this ).toggleClass( "newcell" );
+			});
+
+
+
+
+
+		});

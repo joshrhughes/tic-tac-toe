d8c51308040cacd3bfba512995fec0bbc03299ed
joshrhughes <josh@mtnpine.us>
2017-12-09 08:52:15 -0700

    added vertical win logic

--- js/file.js
+++ js/file.js
@@ -48,6 +48,12 @@ $().ready(function(){
 				alert("X Wins!");  }	
 			if(($('#3').hasClass('xcell')) && ($('#5').hasClass('xcell')) && ($('#7').hasClass('xcell'))) {
 				alert("X Wins!");  }
+			if(($('#1').hasClass('xcell')) && ($('#4').hasClass('xcell')) && ($('#7').hasClass('xcell'))) {
+				alert("X Wins!");  }
+			if(($('#2').hasClass('xcell')) && ($('#5').hasClass('xcell')) && ($('#8').hasClass('xcell'))) {
+				alert("X Wins!");  }
+			if(($('#3').hasClass('xcell')) && ($('6').hasClass('xcell')) && ($('#9').hasClass('xcell'))) {
+				alert("X Wins!");  }
 			//Winning Logic for O
 			if(($('#1').hasClass('ocell')) && ($('#2').hasClass('ocell')) && ($('#3').hasClass('ocell'))) {
 				alert("O Wins!");  }
@@ -58,6 +64,12 @@ $().ready(function(){
 			if(($('#1').hasClass('ocell')) && ($('#5').hasClass('ocell')) && ($('#9').hasClass('ocell'))) {
 				alert("O Wins!");  }	
 			if(($('#3').hasClass('ocell')) && ($('#5').hasClass('ocell')) && ($('#7').hasClass('ocell'))) {
+				alert("O Wins!");  }
+			if(($('#1').hasClass('ocell')) && ($('#4').hasClass('ocell')) && ($('#7').hasClass('ocell'))) {
+				alert("O Wins!");  }
+			if(($('#2').hasClass('ocell')) && ($('#5').hasClass('ocell')) && ($('#8').hasClass('ocell'))) {
+				alert("O Wins!");  }
+			if(($('#3').hasClass('ocell')) && ($('6').hasClass('ocell')) && ($('#9').hasClass('ocell'))) {
 				alert("O Wins!");  }
 			
 	});	
